3c6d3431e9cb370daad084c01d4ef21052530594
CozyBrian <dragonbrian33@gmail.com>
2023-05-23 01:59:20 +0000

    sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import Header from "@/components/layout/header";
 import Image from "next/image";
 import PeavaultSlash from "@/assets/images/peavault-slash.svg";
+import AboutPeavault from "@/assets/images/about-peavault.png";
+import HowItWorks from "@/assets/images/how-it-works.svg";
 import Footer from "@/components/layout/footer";
+import { FeaturesData, HowItWorksData } from "@/constants/overviewData";
 
 export default function Home() {
   return (
@@ -32,14 +35,116 @@ export default function Home() {
           </button>
         </div>
       </section>
-      <div
+      <section
         style={{
           boxShadow: "0px 0px 8px 23px rgba(143, 255, 0, 0.2);",
         }}
-        className="flex flex-col mb-24 items-center justify-center bg-peavault-primary w-full rounded-3xl aspect-video"
-      >
-        lol
-      </div>
+        className="flex flex-col items-center justify-center bg-peavault-primary w-full rounded-3xl aspect-video"
+      ></section>
+      <section className="flex flex-row px-8 my-20 w-full aspect-video">
+        <div className="w-1/2 flex flex-col items-center justify-center gap-10">
+          <div className="flex flex-col gap-10">
+            <div className="flex flex-col gap-1 text-peavault-primary">
+              <p className="text-base font-circular-std">About Peavault</p>
+              <p className="text-5xl font-oatmeal-sans">Buy & Sell With Us</p>
+            </div>
+            <div className="flex flex-col gap-2 text-xl font-circular-std font-light">
+              <p>
+                Our platform is designed to provide a seamless and intuitive
+                user experience, so you can easily set the terms of a
+                transaction, track its progress, and receive notifications when
+                it is complete. Peavault is built on a robust and scalable
+                infrastructure, so you can transact with confidence even as our
+                platform grows in popularity.
+              </p>
+              <p>
+                At Peavault, we believe in the power of trust to drive commerce
+                and build relationships. That&apos;s why our platform is built
+                on trust, so you can transact securely and confidently without
+                the need for intermediaries or third-party services.
+              </p>
+              <p>
+                Whether you&apos;re buying or selling digital assets, goods, or
+                services, Peavault provides a secure and efficient platform for
+                your transactions. With our platform, you can focus on what
+                matters most – building your business and achieving your goals.
+              </p>
+              <p>
+                Join the Peavault community today and experience the future of
+                secure and trustworthy transactions.
+              </p>
+            </div>
+            <div className="flex flex-row gap-2">
+              <button className="h-12 w-32 font-circular-std rounded-lg border-2 border-peavault-primary text-peavault-primary">
+                Start for free
+              </button>
+              <button className="h-12 w-40 rounded-xl bg-peavault-primary text-black">
+                How it Works
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="relative w-1/2">
+          <div className="absolute bottom-0 left-0 w-full h-full">
+            <Image
+              className="h-full"
+              src={AboutPeavault}
+              alt="about-peavault"
+            />
+          </div>
+        </div>
+      </section>
+      <section className="flex flex-col my-20 gap-16 w-full">
+        <div className="flex flex-col items-center gap-2 justify-center font-circular-std text-peavault-primary">
+          <p>Features</p>
+          <p className="text-5xl font-oatmeal-sans">
+            These features makes us the best in the market
+          </p>
+          <p className="text-[#CACACA] text-center max-w-[900px]">
+            Our platform is built on trust and designed to provide a seamless
+            and intuitive user experience, so you can easily transact with
+            confidence. Here are some of the key features of Peavault:
+          </p>
+        </div>
+        <div className="flex flex-row flex-wrap items-center justify-evenly w-full">
+          {FeaturesData.map((feature, i) => (
+            <div
+              key={`feature-${i}`}
+              className="w-72 h-[420px] flex flex-col gap-4 p-6 rounded-2xl border border-peavault-primary"
+            >
+              <div className="flex items-center justify-center w-12 h-12 rounded-[10px] bg-peavault-primary">
+                <Image src={feature.icon} alt={feature.title} />
+              </div>
+              <p className="font-circular-std text-xl text-peavault-primary">
+                {feature.title}
+              </p>
+              <p className="text-base">{feature.description}</p>
+            </div>
+          ))}
+        </div>
+      </section>
+      <section className="flex flex-col my-20 px-4 gap-28 w-full">
+        <div className="flex justify-center w-full">
+          <p className="text-5xl font-oatmeal-sans text-center text-peavault-primary">
+            How It Works
+          </p>
+        </div>
+        <div className="flex flex-row items-center h-[500px] w-full">
+          <div className="h-full w-1/2">
+            <Image src={HowItWorks} alt="how-it-works" />
+          </div>
+          <div className="h-full w-1/2 flex flex-col justify-between">
+            {HowItWorksData.map((data, i) => (
+              <div key={`hiw-${i}`} className="flex flex-col gap-2">
+                <p className="text-xl font-circular-std text-peavault-primary">
+                  {data.title}
+                </p>
+                <p>{data.description}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
       <Footer />
     </main>
   );
---
+++ src/constants/overviewData.ts
@@ -0,0 +1,53 @@
+import HandCoinIcon from "@/assets/icons/hand-coin-icon.svg";
+import PadlockIcon from "@/assets/icons/padlock-icon.svg";
+import ArrowPictureIcon from "@/assets/icons/arrow-picture-icon.svg";
+import ThreeDotMatrixIcon from "@/assets/icons/three-dot-matrix-icon.svg";
+
+export const FeaturesData = [
+  {
+    title: "Trust-based transactions",
+    icon: HandCoinIcon,
+    description:
+      "Our platform is built on trust, allowing you to transact securely and confidently without intermediaries or third-party services. You can set the terms of a transaction, and once both parties have fulfilled their obligations, Peavault automatically executes the transaction.",
+  },
+  {
+    title: "Secure & Efficient",
+    icon: PadlockIcon,
+    description:
+      "Peavault provides a secure and efficient platform for your transactions, whether you're buying or selling digital assets, goods, or services. Our platform uses state-of-the-art encryption and security protocols to protect your data and assets.",
+  },
+  {
+    title: "Intuitive User Experience",
+    icon: ArrowPictureIcon,
+    description:
+      "Our platform is designed to provide a seamless and intuitive user experience, so you can easily set the terms of a transaction, track its progress, and receive notifications when it is complete. Peavault is built on a user-friendly interface that allows you to easily navigate and manage your transactions.",
+  },
+  {
+    title: "Robust & Scalable Infrastructure",
+    icon: ThreeDotMatrixIcon,
+    description:
+      "Peavault is built on a robust and scalable infrastructure, so you can transact with confidence even as our platform grows in popularity. Our platform can handle high volumes of transactions without compromising security or performance.",
+  },
+];
+
+export const HowItWorksData = [
+  {
+    title: "Define the terms",
+    description:
+      "The buyer and seller agree on the terms of the transaction, including the price, delivery, and any other relevant details.",
+  },
+  {
+    title: "Deposit funds",
+    description: "The buyer deposits funds into the Peavault escrow account.",
+  },
+  {
+    title: "Fulfill the terms",
+    description:
+      "The seller fulfills the terms of the transaction, such as delivering the goods or services.",
+  },
+  {
+    title: "Release funds",
+    description:
+      "Once the terms of the transaction are fulfilled, the funds are released to the seller.",
+  },
+];
