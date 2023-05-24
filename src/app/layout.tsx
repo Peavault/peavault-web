ad558fb462d32d9e1797a3290b7b0790231cff77
CozyBrian <dragonbrian33@gmail.com>
2023-05-24 14:32:55 +0000

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,25 @@ import "./globals.css";
 export const metadata = {
   title: "Peavault",
   description: "Later insert peavault description over here",
+  icons: [
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      url: "/favicon/favicon-32x32.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      url: "/favicon/favicon-16x16.png",
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      url: "/favicon/apple-touch-icon.png",
+    },
+  ],
 };
 
 export default function RootLayout({
@@ -16,6 +35,9 @@ export default function RootLayout({
       lang="en"
       className={`${FONTS.oatmeal.variable} ${FONTS.circularStd.variable}`}
     >
+      <head>
+        <link rel="icon" type="image/x-icon" href="/favicon/favicon.ico" />
+      </head>
       <body suppressHydrationWarning={true}>
         <main className="flex flex-col items-center justify-between bg-black w-full min-h-screen px-4 lg:px-28 text-white">
           {children}
